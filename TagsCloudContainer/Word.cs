29ea26020576218a8c83e8739c1c1e34ccbf4fd5
Dmitriy Bessarab <bessarabds@gmail.com>
2024-12-27 14:22:41 +0500

    Word became record

--- TagsCloudContainer/Word.cs
+++ TagsCloudContainer/Word.cs
@@ -1,18 +1,4 @@
 ﻿namespace TagsCloudContainer
 {
-    public class Word(string word)  
-    {
-        public string Value { get; set; } = word;
-
-        public override bool Equals(object? obj)
-        {
-            var other = obj as Word;
-            return Value == other.Value;
-        }
-
-        public override int GetHashCode()
-        {
-            return Value.GetHashCode();
-        }
-    }
+    public record Word(string Value);
 }
