29142c4037f42dafdc309cd6122a27208176f19a
Dmitriy Bessarab <bessarabds@gmail.com>
2025-01-07 19:28:30 +0500

    DocXTextProvider

---
+++ TagsCloudContainer/TextProviders/DocXTextProvider.cs
@@ -0,0 +1,22 @@
+﻿using Xceed.Words.NET;
+
+namespace TagsCloudContainer.TextProviders;
+
+[Label(".docx")]
+public class DocXTextProvider : ITextProvider
+{
+    private readonly string _filePath;
+
+    public DocXTextProvider(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public string ReadFile()
+    {
+        if (!File.Exists(_filePath)) throw new FileNotFoundException();
+        else
+            using (var document = DocX.Load(_filePath))
+                return document.Text;
+    }
+}
